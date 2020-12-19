945f8a004b3247d06bb2c2fd3aa40c4e6ac6180f
garrychen <chenymj23@gmail.com>
2020-12-19 23:40:59 +0800

    lazy initialization

---
+++ src/main/java/com/garry/patterns/thread_safe/initialization/SafeInitializationHolder.java
@@ -0,0 +1,24 @@
+package com.garry.patterns.thread_safe.initialization;
+
+/**
+ * Pattern: Safe Lazy Initialization
+ *
+ * Motivation: Due to the Java Memory Model, some lazy initialization patterns can be unsafe.
+ * Apply the Lazy Initialization idiom correctly using the mechanisms provided by the JVM ???todo
+ *
+ * Intent: Safe initialize our object using the holder idiom.Memory
+ * writes made during static initialization are automatically visible to all threads.
+ *
+ * Applicability: Always when you want to initialize an object in a concurrent code
+ * execution.
+ */
+public class SafeInitializationHolder {
+
+    private static class ResourceHolder {
+        public static Object resource = new Object();
+    }
+
+    public Object getResource(){
+        return ResourceHolder.resource;
+    }
+}
