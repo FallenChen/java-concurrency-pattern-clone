07ef693558e6d5211f7a173049e12d0838a000a6
garrychen <chenymj23@gmail.com>
2020-12-30 22:08:01 +0800

    non_blocking

---
+++ src/main/java/com/garry/patterns/non_blocking/DoubleCounter.java
@@ -0,0 +1,32 @@
+package com.garry.patterns.non_blocking;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Pattern: Non-blocking Design
+ *
+ * Example: Double Counter
+ */
+public class DoubleCounter {
+
+    private AtomicInteger value = new AtomicInteger(0);
+
+    public void increase(){
+        int updatedValue;
+        int old;
+        do {
+            old = value.get(); // get value
+            updatedValue = old + 2;// new updated value
+        }while (!value.compareAndSet(old, updatedValue));
+        /**
+         * if the excepted value (old) differs from what is actually present in the
+         * structure, this means that in the mean time some thread changed it. The loop
+         * will run again until it reached an acceptable state.
+         */
+    }
+
+    public int getValue()
+    {
+        return value.get();
+    }
+}
